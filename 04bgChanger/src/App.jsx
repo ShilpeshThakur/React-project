0d4e638fc9381a0f4c61dae49b95b05489314e93
thakur.shilpesh007@gmail.com <sthakur01@entrata.com>
2023-12-30 22:45:46 +0530

    color change project

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [color, setColor] = useState("olive")
+    //gave call back to onclick. we cant assign function directly
+
+  return (
+
+    <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
+      <div className="fixed  flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-2 rounded-3xl">
+            <button
+                onClick={()=>setColor("red")} className="outline-none px-4 rounded-full test-white shadow-lg"
+            style={{backgroundColor:"red"}}> Red</button>
+        </div>
+          <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-2 rounded-3xl">
+              <button
+                  onClick={()=>setColor("green")} className="outline-none px-4 rounded-full test-white shadow-lg"
+                      style={{backgroundColor:"green"}}> Green</button>
+          </div>
+          <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-2 rounded-3xl">
+              <button
+                  onClick={()=>setColor("blue")} className="outline-none px-4 rounded-full test-white shadow-lg"
+                      style={{backgroundColor:"blue"}}> Blue</button>
+          </div>
+          <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-2 rounded-3xl">
+              <button
+                  onClick={()=>setColor("pink")} className="outline-none px-4 rounded-full test-white shadow-lg"
+                      style={{backgroundColor:"pink"}}> Pink</button>
+          </div>
+          <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-2 rounded-3xl">
+              <button
+                  onClick={()=>setColor("yellow")} className="outline-none px-4 rounded-full test-white shadow-lg"
+                      style={{backgroundColor:"yellow"}}> Yellow</button>
+          </div>
+          <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-2 rounded-3xl">
+              <button
+                  onClick={()=>setColor("black")} className="outline-none px-4 rounded-full test-white shadow-lg"
+                      style={{backgroundColor:"black"}}> Black</button>
+          </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default App
