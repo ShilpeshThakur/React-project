23c311fb23a1425a5bc909871da8dd86a27639a5
thakur.shilpesh007@gmail.com <sthakur01@entrata.com>
2023-12-30 17:56:29 +0530

    custom component

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,11 @@
 
 import Chai from './chai.jsx';
 function App() {
-
+    const userName = "shilpesh"
   return (
       <>
         <Chai />
-        <h1>chai and react</h1>
+        <h1>chai and react {userName}</h1>
         <p> test</p>
 
       </>
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import {jsx as _jsx} from "react/jsx-runtime.js"
 import App from './App.jsx'
+
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom App</h1>
+        </div>
+    )
+}
+
+// const reactElement = {
+//     type : 'a',
+//     props:{
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="httmps://google.com" target="_blank"> visit google</a>
+)
+
+const anotherUser = " Shilpesh"
+
+const reactElement = React.createElement(
+    'a',
+    {href:"https://google.com", target:"_blank"},
+    'click me to visit google',
+    anotherUser
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
+    // MyApp()
+
+  //   anotherElement
+
+    // reactElement
 )
